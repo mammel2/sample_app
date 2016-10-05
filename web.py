f23fc035c5bb2cbdfb06d042be2d08ab062ad03b
Matthew Hammel <matthewjhammel@Matthew-Hammels-MacBook-Pro.local>
2016-10-05 16:46:14 -0400

    Created an about page

--- web.py
+++ web.py
@@ -6,8 +6,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-#runs the new instance
-app.run(debug=True)
+#create about page
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
+app.run(debug=True)
 
 
